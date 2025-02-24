9646f9fa438c6d0b71255a5417ea04955cd16904
bevane <bevane@live.com>
2025-02-24 17:33:10 +0700

    test: add test for insertion sort

---
+++ sort/sort_test.go
@@ -0,0 +1,38 @@
+package sort
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+var testCases = []struct {
+	input []int
+	want  []int
+}{
+	{
+		[]int{9, 8, 7, 6, 5, 4, 3, 2, 1},
+		[]int{1, 2, 3, 4, 5, 6, 7, 8, 9},
+	},
+	{
+		[]int{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		[]int{1, 2, 3, 4, 5, 6, 7, 8, 9},
+	},
+	{
+		[]int{1, 5, 8, 7, 3, 6, 7, 4, 9},
+		[]int{1, 3, 4, 5, 6, 7, 7, 8, 9},
+	},
+}
+
+func TestInsertionSort(t *testing.T) {
+	for _, test := range testCases {
+		t.Run(fmt.Sprintf("%v", test.input), func(t *testing.T) {
+			arr := make([]int, len(test.input))
+			copy(arr, test.input)
+			InsertionSort(arr)
+			if !reflect.DeepEqual(arr, test.want) {
+				t.Errorf("got %v want %v", arr, test.want)
+			}
+		})
+	}
+}
